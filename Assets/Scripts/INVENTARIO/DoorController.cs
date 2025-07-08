f928fff0c1dff9b7660c2aaa1093a03d413ea6b0
Gabriel Ramos <grg2051@outlook.com>
2025-07-07 21:24:10 -0600

    Version 0.22 [Computadoras Aleatorias, Luz no atraviesa Objetos, Estamina]

--- Assets/Scripts/INVENTARIO/DoorController.cs
+++ Assets/Scripts/INVENTARIO/DoorController.cs
@@ -3,28 +3,41 @@ using UnityEngine;
 public class DoorController : MonoBehaviour
 {
     public string requiredKeyID;
-
+    private Animator doorAnim;
     public bool isOpen = false;
+
+    [SerializeField]
+    private bool requiresKey;
+
+    private void Start()
+    {
+        doorAnim = GetComponent<Animator>();
+    }
 
     private void Update()
     {
-        if (isOpen)
-        {
-            Destroy(gameObject);
-        }
-
-        if (Input.GetKeyDown(KeyCode.E) && RayoDetect.Instance.lookingTo == this.gameObject)
+        if (Input.GetKeyDown(KeyCode.E) && RayoDetect.Instance.lookingTo == this.gameObject && !isOpen)
         {
             TryOpen();
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && RayoDetect.Instance.lookingTo == this.gameObject && isOpen)
+        {
+            isOpen = false;
+            doorAnim.SetTrigger("Cerrar");
         }
     }
 
     private void TryOpen()
     {
-        if (!isOpen && Inventario2.Instance.HasKey(requiredKeyID))
+        if (!requiresKey)
         {
             isOpen = true;
-
+            doorAnim.SetTrigger("Abrir");
+        }
+        else if (requiresKey && !isOpen && Inventario2.Instance.HasKey(requiredKeyID))
+        {
+            isOpen = true;
+            doorAnim.SetTrigger("Abrir");
         }
     }
 
